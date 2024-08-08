45210a825011eee6a630e16eb7c1a6d0b72c1f35
tien <nq2017.nguyentien081202@gmail.com>
2024-08-08 16:35:34 +0700

    Add reference for WebAPI

---
+++ ContosoPizza/Models/PizzaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ContosoPizza.Models
+{
+    internal class PizzaContext : DbContext
+    {
+
+        public DbSet<Pizza> Pizzas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build()
+                .GetSection("ConnectionStrings")["mssql"];
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ ContosoPizza/Services/PizzaService.cs
@@ -0,0 +1,46 @@
+﻿using ContosoPizza.Models;
+using Microsoft.EntityFrameworkCore;
+namespace ContosoPizza.Services;
+
+internal static class PizzaService
+{
+    static internal List<Pizza> GetPizzas()
+    {
+        using var db = new PizzaContext();
+        var pizzas = db.Pizzas.ToList();
+        return pizzas;
+    }
+    static internal Pizza Get(int id)
+    {
+        using var db = new PizzaContext();
+        var pizza = db.Pizzas.FirstOrDefault(x => x.Id == id);
+        return pizza; 
+    }
+    static internal void DeletePizza(int id)
+    {
+        var pizza = Get(id);
+        using var db = new PizzaContext();
+        db.Remove(pizza);
+        db.SaveChanges();
+    }
+    static internal void Update(int id, Pizza newPizza)
+    {
+        
+        using var db = new PizzaContext();
+        var pizza = db.Pizzas.FirstOrDefault(x => x.Id == id);
+        db.Pizzas.Attach(pizza);
+        pizza.Name = newPizza.Name;
+        pizza.IsGlutenFree = newPizza.IsGlutenFree;
+
+
+        db.SaveChanges();
+    }
+    static internal void Add(Pizza newPizza)
+    {
+        
+        using var db = new PizzaContext();
+        db.Add(newPizza); 
+        db.SaveChanges();
+    }
+
+}
