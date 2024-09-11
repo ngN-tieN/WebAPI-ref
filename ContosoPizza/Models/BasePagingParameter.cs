a1237e0894cd4ba36b72acf0f9b9916751900428
tien <nq2017.nguyentien081202@gmail.com>
2024-09-11 14:33:20 +0700

    [Update] DTO

---
+++ ContosoPizza/Models/BasePagingParameter.cs
@@ -0,0 +1,9 @@
+﻿namespace ContosoPizza.Models
+{
+    public record BasePagingParameter
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+
+    }
+}
---
+++ ContosoPizza/Models/DTO/UserCredentialsDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ContosoPizza.Models.DTO
+{
+    public record UserCredentialsDTO
+    {
+        public required string Id { get; set; }
+        public int RoleId { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
