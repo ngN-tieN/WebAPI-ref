68b165bc0a8bf30074bde070a56f43f6ff11816a
tien <nq2017.nguyentien081202@gmail.com>
2024-09-11 14:31:37 +0700

    [Update] Add Authorization extension

---
+++ ContosoPizza/Shared.Extensions/AuthorizationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using ContosoPizza.Models.DTO;
+using System.Security.Claims;
+
+namespace ContosoPizza.Shared.Extensions
+{
+    public static class AuthorizationExtensions
+    {
+        public static UserCredentialsDTO? GetUserCredential(this HttpContext httpContext)
+        {
+            var principal = httpContext.User;
+            if(httpContext.User?.Identity?.IsAuthenticated ?? false)
+            {
+                return new UserCredentialsDTO
+                {
+                    Name = principal.FindFirst(ClaimTypes.Name)?.Value ?? string.Empty,
+                    Id = principal.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "0",
+                    Email = principal.FindFirst(ClaimTypes.Email)?.Value ?? string.Empty,
+                    PhoneNumber = principal.FindFirst(ClaimTypes.MobilePhone)?.Value ?? string.Empty,
+                    RoleId = int.TryParse(principal.FindFirst(ClaimTypes.Role)?.Value, out int roleId) ? roleId : 0
+
+                };
+                
+            }
+            return null;
+        } 
+    }
+}
