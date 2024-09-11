9c0ba24576ef21880d5760d0fc9389a72e9ded3d
tien <nq2017.nguyentien081202@gmail.com>
2024-09-11 14:32:03 +0700

    [Feature] Add Service to upload file

---
+++ ContosoPizza/Shared.Utils/FileServices.cs
@@ -0,0 +1,27 @@
+﻿namespace ContosoPizza.Shared.Utils
+{
+    public interface IFileService
+    {
+        Task<bool> SaveFile(IFormFile file, string path, string fileName);
+    }
+    public class FileServices : IFileService
+    {
+        
+
+        public async Task<bool> SaveFile(IFormFile file, string path, string fileName)
+        {
+            if (file.Length > 0)
+            {
+                var filePath = Path.Combine(path, fileName);
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    await file.CopyToAsync(stream);
+                }
+                return true;
+
+            }
+            return false;
+            
+        }
+    }
+}
