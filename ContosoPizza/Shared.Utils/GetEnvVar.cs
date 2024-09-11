23e157ceba7e20293f7fa80ff1ede3f56d5a28c9
tien <nq2017.nguyentien081202@gmail.com>
2024-09-11 14:32:55 +0700

    [Update] Miscellaneous Service changes

---
+++ ContosoPizza/Shared.Utils/GetEnvVar.cs
@@ -0,0 +1,31 @@
+﻿namespace Shared.Utils
+{
+    public class GetEnvVar
+    {
+        private static void Load(string path)
+        {
+            if (!File.Exists(path))
+                return;
+            foreach (var line in File.ReadAllLines(path))
+            {
+                var parts = line.Split('=', 2, StringSplitOptions.RemoveEmptyEntries);
+                Environment.SetEnvironmentVariable(parts[0], parts[1]);
+            }
+        }
+        private static string GetEnvFile()
+        {
+            return Path.Combine(Directory.GetCurrentDirectory(), ".env"); 
+        }
+        /// <summary>
+        /// Input the environment key as argument
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>String type of environment string</returns>
+        public static string GetEnvString(string key)
+        {
+            
+            Load(GetEnvFile());
+            return Environment.GetEnvironmentVariable(key);
+        }
+    }
+}
---
+++ ContosoPizza/Shared.Utils/StaticFileServices.cs
@@ -0,0 +1,10 @@
+﻿namespace ContosoPizza.Shared.Utils
+{
+    public static class StaticFileServices
+    {
+        public static string RandomizeFileName(string extension)
+        {
+            return string.Format("{0}{1}", Path.GetRandomFileName().Replace(".", string.Empty), Path.GetExtension(extension));
+        }
+    }
+}
