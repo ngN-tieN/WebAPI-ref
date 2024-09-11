f0f837611559921d5debf2e4d8a3e3ba77735533
tien <nq2017.nguyentien081202@gmail.com>
2024-09-11 14:37:00 +0700

    [Update] Add builder Services

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,29 +1,89 @@
+using ContosoPizza.Data;
+using ContosoPizza.Features.Pizzas;
 using ContosoPizza.Models;
+using ContosoPizza.RepositoryManager;
+using ContosoPizza.Shared.Utils;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Shared.Utils;
+using System.Text;
 namespace ContosoPizza
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<ContosoPizzaContext>(options => options.UseSqlServer(GetEnvVar.GetEnvString("MSSQL_CONNECTION_STRING")));
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            builder.Services.AddScoped<IPizzaRepository, PizzaRepository>();
+            builder.Services.AddScoped<IFileService, FileServices>();
+            builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ContosoPizzaContext>();
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                            .AddJwtBearer(options =>
+                            {
+                                options.TokenValidationParameters = new TokenValidationParameters
+                                {
+                                    ValidateIssuer = true,
+                                    ValidateAudience = true,
+                                    ValidateLifetime = true,
+                                    ValidateIssuerSigningKey = true,
+                                    ValidIssuer = GetEnvVar.GetEnvString("ISSUER"),
+                                    ValidAudience = GetEnvVar.GetEnvString("AUDIENCE"),
+                                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(GetEnvVar.GetEnvString("JWT_SECRET")))
+                                };
+                            });
 
             // Add services to the container.
-
+            builder.Services.AddHttpContextAccessor();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                {
+                  new OpenApiSecurityScheme
+                  {
+                    Reference = new OpenApiReference
+                    {
+                      Type = ReferenceType.SecurityScheme,
+                      Id = "Bearer"
+                    }
+                   },
+                   new string[] { }
+                 }
+  }             );
+            });
 
             var app = builder.Build();
-
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
 
+                var context = services.GetRequiredService<ContosoPizzaContext>();
+                context.Database.EnsureCreated();
+                PopulateDb.Populate(context);
+            }
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
